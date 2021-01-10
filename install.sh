7a618c354146362a80a350ca72da3caa3954c3fa
FoxMaccloud <35312332+FoxMaccloud@users.noreply.github.com>
2021-01-10 21:47:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 printf "\nWelcome!\n\n"
 printf "Installing my dotfiles now! Make sure to be running as root.\n"
-printf "Note that this program is still work in progress!\n\n"
+printf "Note that this program is still work in progress!\n"
+printf "I strongly suggest installing manually for the moment!\n\n"
 while true; do
     read -p "Do you still wish to continue? (y/n)" yn
     case $yn in
@@ -21,7 +22,7 @@ checkInstalled() {
     if type -p "$1" > /dev/null; then
         printf "Found "$1"\n"
     else
-        printf "Installing "$1"\n" && pacman --noconfirm --needed -S "$1"
+        printf "Installing "$1"\n" && pacman --noconfirm --needed -S "$1" 2>/dev/null
     fi
 }
 
@@ -38,7 +39,7 @@ USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 if type -p zsh > /dev/null; then
     printf "Found zsh\n"
 else
-    printf "Installing zsh\n" && pacman --noconfirm --needed -S zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-theme-powerlevel10K
+    printf "Installing zsh\n" && pacman --noconfirm --needed -S zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-theme-powerlevel10K 2>/dev/null
     # Setting zsh as default shell
     chsh -s /bin/zsh "$name" > /dev/null 2>&1
     sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
@@ -78,15 +79,15 @@ mv $USER_HOME/.oh-my-zsh $USER_HOME/.oh-my-zsh-BACKUP 2>/dev/null
 mv $USER_HOME/.zshrc $USER_HOME/.zshrc-BACKUP 2>/dev/null
 mv $USER_HOME/.vimrc $USER_HOME/.vimrc-BACKUP 2>/dev/null
 
-cp -r .config/i3 $USER_HOME/.config/i3 && sed -i "s/home\/foxmaccloud/$USER_HOME/g" $USER_HOME/.config/i3/config
-cp -r .config/picom $USER_HOME/.config/picom
-cp -r .config/polybar $USER_HOME/.config/polybar
-cp -r .config/kitty $USER_HOME/.config/kitty
-cp -r .config/dunst $USER_HOME/.config/dunst
-cp -r .config/ranger $USER_HOME/.config/ranger
-cp -r .oh-my-zsh $USER_HOME/.oh-my-zsh
-cp -r .zshrc $USER_HOME/.zshrc && sed -i "s/home\/foxmaccloud/$USER_HOME/g" $USER_HOME/.zshrc
-cp -r .vimrc $USER_HOME/.vimrc
+cp -r .config/i3 $USER_HOME/.config/i3 && sed -i "s/home\/foxmaccloud/$USER_HOME/g" $USER_HOME/.config/i3/config 2>/dev/null
+cp -r .config/picom $USER_HOME/.config/picom 2>/dev/null
+cp -r .config/polybar $USER_HOME/.config/polybar 2>/dev/null
+cp -r .config/kitty $USER_HOME/.config/kitty 2>/dev/null
+cp -r .config/dunst $USER_HOME/.config/dunst 2>/dev/null
+cp -r .config/ranger $USER_HOME/.config/ranger 2>/dev/null
+cp -r .oh-my-zsh $USER_HOME/.oh-my-zsh 2>/dev/null
+cp -r .zshrc $USER_HOME/.zshrc && sed -i "s/home\/foxmaccloud/$USER_HOME/g" $USER_HOME/.zshrc 2>/dev/null
+cp -r .vimrc $USER_HOME/.vimrc 2>/dev/null
 
 # Setting up wallpapers
 if [! -f $USER_HOME/Pictures/Wallpapers]; then
